ba25c6f61e4c4d0d3e1e74be40d2881e10ce7b39
Bellér Csaba <beller.csaba911@gmail.com>
2023-09-24 22:51:06 +0200

    Fixed state management error

--- App/lib/src/ui/pages/random_joke/random_joke_page.dart
+++ App/lib/src/ui/pages/random_joke/random_joke_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:forest_logger/forest_logger.dart';
 import 'package:jokes_app/src/models/joke/joke_model.dart';
 import 'package:jokes_app/src/providers/joke/joke_provider.dart';
+import 'package:jokes_app/src/resources/theme/application_style.dart';
 import 'package:jokes_app/src/ui/elements/custom_joke_item.dart';
 import 'package:jokes_app/src/ui/widgets/custom_appbar.dart';
 import 'package:provider/provider.dart';
@@ -22,31 +23,43 @@ class _RandomJokePageState extends State<RandomJokePage> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      jokes = Provider.of<JokeProvider>(context, listen: false).jokeModels;
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await Provider.of<JokeProvider>(context, listen: false)
+          .getJokesByCategory("any", 10);
       Forest.debug(jokes!.length.toString());
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    jokes = Provider.of<JokeProvider>(context, listen: false).jokeModels;
+
     return Scaffold(
       appBar: CustomAppbar(
         scaffoldKey: scaffoldKey,
         title: "Random jokes",
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return CustomJokeItem(
-            joke: jokes?[index].joke,
-            delivery: jokes?[index].delivery,
-            setup: jokes?[index].setup,
-            isFavorite: false,
-            onTapFavorite: () {},
-          );
-        },
-      ),
+      body: Provider.of<JokeProvider>(context, listen: true).isLoading
+          ? Container(
+              color: ApplicationStyle.primaryColor,
+              child: const Center(
+                child: CircularProgressIndicator(
+                  color: ApplicationStyle.textColor,
+                ),
+              ),
+            )
+          : ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return CustomJokeItem(
+                  joke: jokes?[index].joke,
+                  delivery: jokes?[index].delivery,
+                  setup: jokes?[index].setup,
+                  isFavorite: false,
+                  onTapFavorite: () {},
+                );
+              },
+            ),
     );
   }
 }
